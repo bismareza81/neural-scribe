786e17e119c5b15bbc096ffe0029b33595c6fbaa
bismareza81 <bismareza@outlook.com>
2026-04-20 14:20:22 +0700

    update node sveltejs

--- build/server/chunks/1-_5YcS1F-.js
+++
@@ -1,9 +0,0 @@
-const index = 1;
-let component_cache;
-const component = async () => component_cache ??= (await import('./error.svelte-CpfTwCQ7.js')).default;
-const imports = ["_app/immutable/nodes/1.IQzarTTY.js","_app/immutable/chunks/CIjs2MpQ.js","_app/immutable/chunks/D2Yxezly.js","_app/immutable/chunks/CDTg-29K.js"];
-const stylesheets = [];
-const fonts = [];
-
-export { component, fonts, imports, index, stylesheets };
-//# sourceMappingURL=1-_5YcS1F-.js.map
--- svelte.config.js
+++ svelte.config.js
@@ -1,15 +1,12 @@
-import adapter from '@sveltejs/adapter-node'; // Baris ini yang sebelumnya error
-import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+import adapter from '@sveltejs/adapter-static';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	preprocess: vitePreprocess(),
-
-	kit: {
-		// adapter-auto hanya untuk environment tertentu, 
-		// untuk cPanel kita wajib pakai adapter-node
-		adapter: adapter()
-	}
+  kit: {
+    adapter: adapter({
+      fallback: 'index.html'
+    })
+  }
 };
 
 export default config;
